403b9a322d84dd30011c09fcdbdac091c28cee8b
TheChildWalrus <jakegerzimbke@live.co.uk>
2014-06-21 22:01:12 +0100

    Updated map

--- LOTR/src/main/java/lotr/common/world/feature/LOTRWorldGenBaobab.java
+++ LOTR/src/main/java/lotr/common/world/feature/LOTRWorldGenBaobab.java
@@ -97,10 +97,10 @@ public class LOTRWorldGenBaobab extends WorldGenAbstractTree
 				float angleR = (float)Math.toRadians(angle);
 				float sin = MathHelper.sin(angleR);
 				float cos = MathHelper.cos(angleR);
-				int boughLength = 4 + random.nextInt(8);
+				int boughLength = 8 + random.nextInt(8);
 				int boughThickness = Math.round((float)boughLength / 30F);
-				int boughBaseHeight = j + MathHelper.floor_double(height * (0.85F + random.nextFloat() * 0.15F));
-				int boughHeight = 1 + random.nextInt(4);
+				int boughBaseHeight = j + MathHelper.floor_double(height * (0.75F + random.nextFloat() * 0.25F));
+				int boughHeight = 5 + random.nextInt(7);
 				
 				for (int l = 0; l < boughLength; l++)
 				{
@@ -123,27 +123,54 @@ public class LOTRWorldGenBaobab extends WorldGenAbstractTree
 							}
 						}
 					}
-
-					if (l == boughLength - 1)
+					
+					if (l < boughLength / 2)
 					{
-						int leafRange = 3;
+						continue;
+					}
+					
+					int branch_angle = angle + random.nextInt(360);
+					float branch_angleR = (float)Math.toRadians(branch_angle);
+					float branch_sin = MathHelper.sin(branch_angleR);
+					float branch_cos = MathHelper.cos(branch_angleR);
+					int branchLength = 6 + random.nextInt(6);
+					int branchHeight = 5 + random.nextInt(5);
+					int leafRange = 3;
+					
+					for (int l1 = 0; l1 < branchLength; l1++)
+					{
+						int i2 = i1 + Math.round(branch_sin * l1);
+						int k2 = k1 + Math.round(branch_cos * l1);
+						int j2 = j1 + Math.round((float)l1 / (float)branchLength * (float)branchHeight);
 						
-						for (int i2 = i1 - leafRange; i2 <= i1 + leafRange; i2++)
+						for (int j3 = j2; j3 >= j2 - 1; j3--)
 						{
-							for (int j2 = j1 - leafRange; j2 <= j1 + leafRange; j2++)
+							Block block = world.getBlock(i2, j3, k2);
+							if (block.getMaterial() == Material.air || block.isLeaves(world, i2, j3, k2))
 							{
-								for (int k2 = k1 - leafRange; k2 <= k1 + leafRange; k2++)
+								setBlockAndNotifyAdequately(world, i2, j3, k2, woodBlock, woodMeta | 12);
+							}
+						}
+						
+						if (l1 == branchLength - 1)
+						{
+							for (int i3 = i2 - leafRange; i3 <= i2 + leafRange; i3++)
+							{
+								for (int j3 = j2 - leafRange; j3 <= j2 + leafRange; j3++)
 								{
-									int i3 = i2 - i1;
-									int j3 = j2 - j1;
-									int k3 = k2 - k1;
-									int dist = i3 * i3 + j3 * j3 + k3 * k3;
-									if (dist < (leafRange - 1) * (leafRange - 1) || (dist < leafRange * leafRange && random.nextInt(3) != 0))
+									for (int k3 = k2 - leafRange; k3 <= k2 + leafRange; k3++)
 									{
-										Block block2 = world.getBlock(i2, j2, k2);
-										if (block2.getMaterial() == Material.air || block2.isLeaves(world, i2, j2, k2))
+										int i4 = i3 - i2;
+										int j4 = j3 - j2;
+										int k4 = k3 - k2;
+										int dist = i4 * i4 + j4 * j4 + k4 * k4;
+										if (dist < (leafRange - 1) * (leafRange - 1) || (dist < leafRange * leafRange && random.nextInt(3) != 0))
 										{
-											setBlockAndNotifyAdequately(world, i2, j2, k2, leafBlock, leafMeta);
+											Block block2 = world.getBlock(i3, j3, k3);
+											if (block2.getMaterial() == Material.air || block2.isLeaves(world, i3, j3, k3))
+											{
+												setBlockAndNotifyAdequately(world, i3, j3, k3, leafBlock, leafMeta);
+											}
 										}
 									}
 								}
